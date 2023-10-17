7012e6921e6021c765a92b253fa2e72cdac63557
indugaor <185240@ust.com>
2023-10-17 17:51:22 +0530

    first commit

---
+++ dialywork/Calculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Programs
+{
+     class Calculation
+    {
+        double num1, num2, ans;
+        public double Add(int num1,int num2)
+        {
+            ans = num1 + num2;
+            Console.WriteLine(ans);
+            return ans;
+        }
+
+    }
+}
---
+++ dialywork/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Console.WriteLine("Enter 2 numbers");
+int num1, num2, ans;
+num1 = Convert.ToInt32(Console.ReadLine());
+num2 = Convert.ToInt32(Console.ReadLine());
+ans = num1 + num2;
+Console.WriteLine(ans);
+*/
+/*
+string s1 = "";
+string s2 = "";
+string s3;
+s1 = Console.ReadLine();
+s2 = Console.ReadLine();
+s3 = s1 + " " + s2;
+Console.WriteLine(s1.Equals(s2));
+Console.WriteLine(s1.ToUpper());
+Console.WriteLine(s1.ToLower());
+Console.WriteLine(s2.Substring(3));
+string[] s = s3.Split(' ');
+for (int i = 0; i < s.Length; i++)
+    Console.WriteLine(s[i]);
+*/
+/*
+int x = 5;
+switch (x)
+{
+    case 1:
+        Console.WriteLine();
+        break;
+    case 2:
+        Console.WriteLine();
+        break;
+    case 3:
+        Console.WriteLine();
+        break;
+    default:
+        Console.WriteLine();
+        break;
+
+}
+*/
+//driver
+using Basic_Programs;
+Calculation calculation = new Calculation();
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+ double res=calculation.Add(num1,num2);
+Console.WriteLine(res);
+
+
