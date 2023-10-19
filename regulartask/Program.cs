dac96caea57691d44c604d39a0efdf33b3192f08
indugaor <185240@ust.com>
2023-10-19 16:53:49 +0530

    first commit

---
+++ regulartask/Program.cs
@@ -0,0 +1,4 @@
+﻿int[] studentGrade = { 88, 76, 98, 67 };
+regular.Student student = new regular.Student("Indu", studentGrade);
+double average = student.CalculateAverage();
+Console.WriteLine($"{student.studentName}'s average grade is {average}");
---
+++ regulartask/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace regular
+{
+    class Student
+    {
+        public string studentName;
+        public int[] studentGrades;
+        public Student(string studentName, int[] studentGrades)
+        {
+            this.studentName = studentName;
+            this.studentGrades = studentGrades;
+
+
+        }
+        public double CalculateAverage()
+        {
+            if (studentGrades.Length == 0)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+            foreach (int grade in studentGrades)
+            {
+                sum += grade;
+
+            }
+            return sum / studentGrades.Length;
+        }
+    }
+}
